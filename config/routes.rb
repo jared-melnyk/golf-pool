19285a921a8b73e52d07295380c3c5adeda99170
Jared Melnyk <jared.melnyk@gmail.com>
2026-03-01 11:31:12 -0600

    chore: remove manual add golfer UI; golfers from API/sync only

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   resources :tournaments, only: [ :index, :new, :create, :show ] do
     resources :tournament_results, only: [ :create, :destroy ], path: "results"
   end
-  resources :golfers, only: [ :index, :new, :create ]
+  resources :golfers, only: [ :index ]
 
   get "sync", to: "sync#index", as: :sync_index
   post "sync/tournaments", to: "sync#tournaments"
