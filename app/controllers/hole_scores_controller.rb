9a35492bd9b54ab93175395cdade2b9948f94414
Jared Melnyk <jared.melnyk@gmail.com>
2026-05-11 14:23:08 -0500

    fix: players may only edit their own scores; submitted games are locked

--- app/controllers/hole_scores_controller.rb
+++ app/controllers/hole_scores_controller.rb
@@ -4,13 +4,11 @@ class HoleScoresController < ApplicationController
   before_action :set_event
   before_action :set_game
   before_action :require_event_member!
-  before_action :require_score_entry_permitted!
+  before_action :require_game_not_submitted!
+  before_action :set_and_authorize_gtp
 
   def update
-    gtp = GameTeamPlayer.joins(:game_team)
-                        .where(game_teams: { game_id: @game.id })
-                        .find(params[:id])
-    score = gtp.hole_scores.find_or_initialize_by(hole_number: params[:hole_number].to_i)
+    score = @gtp.hole_scores.find_or_initialize_by(hole_number: params[:hole_number].to_i)
     gross = params[:gross_score].to_s.strip
 
     if gross.blank?
@@ -41,10 +39,24 @@ class HoleScoresController < ApplicationController
     redirect_to event_path(@event), alert: "You must be a member of this event."
   end
 
-  def require_score_entry_permitted!
-    return if @event.commissioner?(current_user)
-    return if @game.game_teams.joins(:game_team_players).where(game_team_players: { user_id: current_user.id }).exists?
+  def require_game_not_submitted!
+    return unless @game.submitted
 
-    redirect_to event_game_path(@event, @game), alert: "You are not a participant in this game."
+    redirect_to event_game_path(@event, @game), alert: "This game has been submitted and is locked."
+  end
+
+  def set_and_authorize_gtp
+    # Scope to this game first (prevents cross-game tampering)
+    @gtp = GameTeamPlayer.joins(:game_team)
+                         .where(game_teams: { game_id: @game.id })
+                         .find(params[:id])
+
+    # Commissioners may edit any player's scores
+    return if @event.commissioner?(current_user)
+
+    # Players may only edit their own row
+    return if @gtp.user_id == current_user.id
+
+    redirect_to event_game_path(@event, @game), alert: "You can only enter your own scores."
   end
 end
